04a3ab57b948b8ecd28ebbba0382b5d6bd37d85e
Thibault Clérice <leponteineptique@gmail.com>
2015-03-19 16:06:12 +0100

    Typeahead update with 1.0.6

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       "./node_modules/typeahead.js/dist/typeahead.bundle.min.js",
       "node_modules/jasmine-ajax/lib/mock-ajax.js",
       "node_modules/jasmine-jquery/lib/jasmine-jquery.js",
-      "node_modules/capitains-sparrow/build/cts.min.js",
+      "node_modules/capitains-sparrow/dist/cts.min.js",
       'spec/javascripts/.helper.js',
 
       //jQuery Plugins
